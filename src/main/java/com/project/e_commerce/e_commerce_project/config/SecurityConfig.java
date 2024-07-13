4fefc6c742735a400f1a0d892a7f39a7cb0ed780
Mustafa-Ashraf <mustafaashraf123@yahoo.com>
2024-07-13 09:05:06 +0300

    Config my FilterChain class

--- src/main/java/com/project/e_commerce/e_commerce_project/config/SecurityConfig.java
+++ src/main/java/com/project/e_commerce/e_commerce_project/config/SecurityConfig.java
@@ -1,59 +1,41 @@
 package com.project.e_commerce.e_commerce_project.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-  private PasswordEncoder passwordEncoder;
-
-  @Autowired
-  public SecurityConfig(PasswordEncoder passwordEncoder) {
-    this.passwordEncoder = passwordEncoder;
-  }
+  private final JWTAuthenticationFilter jwtAuthFilter;
+  private final AuthenticationProvider authenticationProvider;
 
   @Bean
-  public InMemoryUserDetailsManager userDetailsManager(){
-    UserDetails mustafa = User.builder().
-            username("mustafa")
-            .password(passwordEncoder.encode("123"))
-            .roles("ADMIN","USER")
-            .build();
-
-    UserDetails ahmed = User.builder().
-            username("ahmed")
-            .password(passwordEncoder.encode("555"))
-            .roles("USER")
-            .build();
-    return new InMemoryUserDetailsManager(mustafa,ahmed);
-  }
-
-
-
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf().disable()
-            .authorizeHttpRequests(configurer ->
-                    configurer
-                            .requestMatchers(HttpMethod.GET, "/api/**").permitAll() // Allow GET requests to /api/** without authentication
-                            .requestMatchers(HttpMethod.POST, "/api/admin/add").hasRole("ADMIN") // Only ADMIN can add
-                            .requestMatchers(HttpMethod.POST, "/api/**").authenticated() // All POST requests to /api/** require authentication
-                            .requestMatchers(HttpMethod.PUT, "/api/**").authenticated() // All PUT requests to /api/** require authentication
-                            .requestMatchers(HttpMethod.DELETE, "/api/**").authenticated() // All DELETE requests to /api/** require authentication
-                            .anyRequest().authenticated() // Any other requests require authentication
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
+                    httpRequest ->
+                            httpRequest.requestMatchers("/api/register","/api/login").
+                                    permitAll()
+                                    .anyRequest().
+                                    authenticated()
             )
-            .httpBasic(); // Use Basic authentication
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthFilter, BasicAuthenticationFilter.class);
 
     return http.build();
   }
+
 
 }
